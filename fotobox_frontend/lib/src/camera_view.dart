a41e858134a9b484dff4302d975be3ba74002821
fseitz <fabi.seitz@gmail.com>
2024-07-26 21:37:03 +0200

    show captured image

--- fotobox_frontend/lib/src/camera_view.dart
+++ fotobox_frontend/lib/src/camera_view.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
 import 'package:camera/camera.dart';
 import 'package:download/download.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:fotobox_frontend/src/manager/session_manager.dart';
 import 'package:intl/intl.dart';
+import 'package:watch_it/watch_it.dart';
 
-class CameraApp extends StatefulWidget {
+class CameraApp extends StatefulWidget with WatchItStatefulWidgetMixin {
   /// Default Constructor
   const CameraApp({
     super.key,
@@ -16,49 +20,89 @@ class CameraApp extends StatefulWidget {
   State<CameraApp> createState() => _CameraAppState();
 }
 
-class _CameraAppState extends State<CameraApp> {
+class _CameraAppState extends State<CameraApp> with WidgetsBindingObserver {
   late CameraController controller;
   String error = '';
+  Timer? timer;
 
   @override
   void initState() {
     super.initState();
-    controller = CameraController(
-      widget.cameras[0],
-      ResolutionPreset.max,
+    WidgetsBinding.instance.addObserver(this);
+
+    _initializeCameraController(widget.cameras[0]);
+  }
+
+  Future<void> _initializeCameraController(
+      CameraDescription cameraDescription) async {
+    final CameraController cameraController = CameraController(
+      cameraDescription,
+      kIsWeb ? ResolutionPreset.max : ResolutionPreset.medium,
+      enableAudio: false,
+      // imageFormatGroup: ImageFormatGroup.jpeg,
     );
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
+
+    controller = cameraController;
+
+    // If the controller is updated then update the UI.
+    cameraController.addListener(() {
+      if (mounted) {
+        setState(() {
+          error = '';
+        });
       }
+      if (cameraController.value.hasError) {
+        error = 'Camera error ${cameraController.value.errorDescription}';
+      }
+    });
+
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      switch (e.code) {
+        case 'CameraAccessDenied':
+          error = 'You have denied camera access.';
+        case 'AudioAccessDenied':
+          error = 'You have denied audio access.';
+        default:
+          error = e.description ?? 'Error';
+          break;
+      }
+    }
+
+    if (mounted) {
       setState(() {
         error = '';
       });
-    }).catchError((Object e) {
-      if (e is CameraException) {
-        switch (e.code) {
-          case 'CameraAccessDenied':
-            setState(() {
-              error = "Error: Access to camera denied";
-            });
-            break;
-          default:
-            setState(() {
-              error = "Error during camera initialization";
-            });
-            break;
-        }
-      }
-    });
+    }
   }
+
+  // #docregion AppLifecycle
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController? cameraController = controller;
+
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      _initializeCameraController(cameraController.description);
+    }
+  }
+  // #enddocregion AppLifecycle
 
   @override
   void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
     controller.dispose();
     super.dispose();
   }
 
-    Future<XFile?> takePicture() async {
+  Future<XFile?> takePicture() async {
     final CameraController cameraController = controller;
     if (!cameraController.value.isInitialized) {
       return null;
@@ -79,6 +123,10 @@ class _CameraAppState extends State<CameraApp> {
 
   @override
   Widget build(BuildContext context) {
+    var lastImage =
+        watchPropertyValue((SessionManager manager) => manager.lastImage);
+    final SessionManager manager = di<SessionManager>();
+
     if (!controller.value.isInitialized) {
       if (error.isNotEmpty) {
         return Center(
@@ -103,7 +151,10 @@ class _CameraAppState extends State<CameraApp> {
       );
     }
 
-    return Column(
+    Widget? image =
+        lastImage != null ? Center(child: Image.network(lastImage.path)) : null;
+
+    var preview = Column(
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -113,11 +164,18 @@ class _CameraAppState extends State<CameraApp> {
                 takePicture().then((XFile? file) async {
                   if (mounted) {
                     if (file != null) {
-                      var time = DateTime.now();
-                      String formattedDate = DateFormat('ddMMyyyy_HHmmss').format(time);
-                      var image = await file.readAsBytes();
-                      var test = Stream.fromIterable(image);
-                      download(test, 'Foto_$formattedDate.png');
+                      // var time = DateTime.now();
+                      // String formattedDate =
+                      //     DateFormat('ddMMyyyy_HHmmss').format(time);
+                      // var image = await file.readAsBytes();
+                      // var test = Stream.fromIterable(image);
+                      // download(test, 'Foto_$formattedDate.png');
+
+                      manager.lastImage = file;
+                      timer = Timer(const Duration(seconds: 3), () {
+                        timer = null;
+                        manager.lastImage = null;
+                      });
                     }
                   }
                 });
@@ -133,6 +191,14 @@ class _CameraAppState extends State<CameraApp> {
           ),
         ),
       ],
+    );
+
+    List<Widget> stackChikdren = [preview];
+    if (image != null) {
+      stackChikdren.add(image);
+    }
+    return Stack(
+      children: stackChikdren,
     );
   }
 }
