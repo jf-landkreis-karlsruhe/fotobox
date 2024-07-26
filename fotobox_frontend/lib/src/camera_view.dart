f6c6d7a99f0ddf7c06e886cdde3060fcaab4f469
fseitz <fabi.seitz@gmail.com>
2024-07-27 00:10:29 +0200

    disable buttons

--- fotobox_frontend/lib/src/camera_view.dart
+++ fotobox_frontend/lib/src/camera_view.dart
@@ -206,16 +206,20 @@ class _CameraAppState extends State<CameraApp> with WidgetsBindingObserver {
             ),
             const SizedBox(width: 10),
             IconButton(
-              onPressed: () {
-                manager.saveCurrentSessionCommand();
-              },
+              onPressed: pictureButtonWasPressed
+                  ? null
+                  : () {
+                      manager.saveCurrentSessionCommand();
+                    },
               icon: const Icon(Icons.save_outlined),
             ),
             const SizedBox(width: 10),
             IconButton(
-              onPressed: () {
-                manager.endCurrentSessionCommand();
-              },
+              onPressed: pictureButtonWasPressed
+                  ? null
+                  : () {
+                      manager.endCurrentSessionCommand();
+                    },
               icon: const Icon(Icons.close),
             ),
           ],
