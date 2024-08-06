671c4a1c1a0445b458ee97cc9fe0e2da66639c29
fseitz <fabi.seitz@gmail.com>
2024-08-06 19:10:09 +0200

    use enter key to start new session

--- fotobox_frontend/lib/src/new_session_view.dart
+++ fotobox_frontend/lib/src/new_session_view.dart
@@ -1,35 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fotobox_frontend/src/manager/session_manager.dart';
 import 'package:watch_it/watch_it.dart';
 
-class NewSessionView extends StatelessWidget with WatchItMixin {
+class NewSessionView extends StatefulWidget with WatchItStatefulWidgetMixin {
   const NewSessionView({super.key});
+
+  @override
+  State<NewSessionView> createState() => _NewSessionViewState();
+}
+
+class _NewSessionViewState extends State<NewSessionView> {
+  var focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     var manager = di<SessionManager>();
-    return Center(
-      child: ElevatedButton.icon(
-        icon: const Icon(Icons.cameraswitch_outlined),
-        label: const Padding(
-          padding: EdgeInsets.only(left: 10),
-          child: Text(
-            'New Session',
-            style: TextStyle(
-              fontSize: 25,
-              color: Colors.white,
+    return KeyboardListener(
+      focusNode: focusNode,
+      autofocus: true,
+      onKeyEvent: (value) {
+        if (value.logicalKey == LogicalKeyboardKey.enter) {
+          manager.startNewSessionCommand();
+        }
+      },
+      child: Center(
+        child: ElevatedButton.icon(
+          icon: const Icon(Icons.cameraswitch_outlined),
+          label: const Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: Text(
+              'New Session',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+              ),
             ),
           ),
+          style: const ButtonStyle(
+            padding: WidgetStatePropertyAll(EdgeInsets.all(20)),
+            iconSize: WidgetStatePropertyAll(60),
+            backgroundColor: WidgetStatePropertyAll(Colors.blue),
+            iconColor: WidgetStatePropertyAll(Colors.white),
+          ),
+          onPressed: () {
+            manager.startNewSessionCommand();
+          },
         ),
-        style: const ButtonStyle(
-          padding: WidgetStatePropertyAll(EdgeInsets.all(20)),
-          iconSize: WidgetStatePropertyAll(60),
-          backgroundColor: WidgetStatePropertyAll(Colors.blue),
-          iconColor: WidgetStatePropertyAll(Colors.white),
-        ),
-        onPressed: () {
-          manager.startNewSessionCommand();
-        },
       ),
     );
   }
