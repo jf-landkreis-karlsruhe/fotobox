f331c7f361744d35d052146d2188bbf095df94ee
NilsHeinzelmann <123253152+NilsHeinzelmann@users.noreply.github.com>
2024-10-27 13:06:39 +0100

    Added URL support, changed Style
    Added URL support for using the frontend. Added JF Logo to the webpage.

--- fotobox_backend/src/main/java/de/fw/fotobox/controller/SessionController.java
+++ fotobox_backend/src/main/java/de/fw/fotobox/controller/SessionController.java
@@ -18,91 +18,90 @@ public class SessionController {
 
 	@Value("${folder.path}")
 	private String storagePath;
-	
+
 	@Value("${cryption.key}")
 	private String cryptionKey;
-	
+
 	@Value("${file.type}")
-    private String fileType;
-	
-	
+	private String fileType;
+
 	private static final String ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    private static final int CODE_LENGTH = 8;
+	private static final int CODE_LENGTH = 8;
 
-    @PostMapping
-    public String saveSession(@RequestBody SessionRequest sessionRequest, HttpServletResponse response) {
-        String token = sessionRequest.getToken();
-        if (!cryptionKey.equals(token)) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            return "Ungültiger Schlüssel";
-        }
-        
-        List<List<Byte>> images = sessionRequest.getImages();
+	@PostMapping
+	public String saveSession(@RequestBody SessionRequest sessionRequest, HttpServletResponse response) {
+		String token = sessionRequest.getToken();
+		if (!cryptionKey.equals(token)) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			return "Ungültiger Schlüssel";
+		}
 
-        String uniqueCode = generateUniqueCode();
+		List<List<Byte>> images = sessionRequest.getImages();
 
-        Path uniqueFolderPath = Path.of(storagePath, uniqueCode);
-        File uniqueFolder = new File(uniqueFolderPath.toString());
-        if (!uniqueFolder.exists() && !uniqueFolder.mkdirs()) {
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            return "Fehler beim Erstellen des Ordners";
-        }
+		String uniqueCode = generateUniqueCode();
 
-        for (int i = 0; i < images.size(); i++) {
-            List<Byte> imageBytes = images.get(i);
-            File imageFile = new File(uniqueFolder, "image_" + (i + 1) + "." + fileType);
+		Path uniqueFolderPath = Path.of(storagePath, uniqueCode);
+		File uniqueFolder = new File(uniqueFolderPath.toString());
+		if (!uniqueFolder.exists() && !uniqueFolder.mkdirs()) {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return "Fehler beim Erstellen des Ordners";
+		}
 
-            try (FileOutputStream fos = new FileOutputStream(imageFile)) {
-                for (byte b : imageBytes) {
-                    fos.write(b);
-                }
-            } catch (IOException e) {
-                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                return "Fehler beim Speichern von Bild " + (i + 1);
-            }
-        }
+		for (int i = 0; i < images.size(); i++) {
+			List<Byte> imageBytes = images.get(i);
+			File imageFile = new File(uniqueFolder, "image_" + (i + 1) + "." + fileType);
 
-        response.setStatus(HttpServletResponse.SC_OK);
-        return uniqueCode;
-    }
+			try (FileOutputStream fos = new FileOutputStream(imageFile)) {
+				for (byte b : imageBytes) {
+					fos.write(b);
+				}
+			} catch (IOException e) {
+				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				return "Fehler beim Speichern von Bild " + (i + 1);
+			}
+		}
 
-    private String generateUniqueCode() {
-        String code;
-        File uniqueFolder;
-        do {
-            code = generateShortCode();
-            uniqueFolder = new File(storagePath, code);
-        } while (uniqueFolder.exists());
-        return code;
-    }
+		response.setStatus(HttpServletResponse.SC_OK);
+		return "localhost:8080/?code=" + uniqueCode;
+	}
 
-    private String generateShortCode() {
-        SecureRandom random = new SecureRandom();
-        StringBuilder code = new StringBuilder(CODE_LENGTH);
-        for (int i = 0; i < CODE_LENGTH; i++) {
-            code.append(ALPHANUMERIC.charAt(random.nextInt(ALPHANUMERIC.length())));
-        }
-        return code.toString();
-    }
+	private String generateUniqueCode() {
+		String code;
+		File uniqueFolder;
+		do {
+			code = generateShortCode();
+			uniqueFolder = new File(storagePath, code);
+		} while (uniqueFolder.exists());
+		return code;
+	}
 
-    public static class SessionRequest {
-        private String token;
-        private List<List<Byte>> images;
+	private String generateShortCode() {
+		SecureRandom random = new SecureRandom();
+		StringBuilder code = new StringBuilder(CODE_LENGTH);
+		for (int i = 0; i < CODE_LENGTH; i++) {
+			code.append(ALPHANUMERIC.charAt(random.nextInt(ALPHANUMERIC.length())));
+		}
+		return code.toString();
+	}
 
-        public String getToken() {
-            return token;
-        }
+	public static class SessionRequest {
+		private String token;
+		private List<List<Byte>> images;
 
-        public void setToken(String token) {
-            this.token = token;
-        }
+		public String getToken() {
+			return token;
+		}
 
-        public List<List<Byte>> getImages() {
-            return images;
-        }
+		public void setToken(String token) {
+			this.token = token;
+		}
 
-        public void setImages(List<List<Byte>> images) {
-            this.images = images;
-        }
-    }
+		public List<List<Byte>> getImages() {
+			return images;
+		}
+
+		public void setImages(List<List<Byte>> images) {
+			this.images = images;
+		}
+	}
 }
