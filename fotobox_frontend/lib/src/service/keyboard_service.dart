09d2da299f161647ad393030460a5c1bc3fe7593
Arne Maier <Arne_Maier@gmx.de>
2025-01-17 21:27:39 +0100

    Add import

--- fotobox_frontend/lib/src/service/keyboard_service.dart
+++ fotobox_frontend/lib/src/service/keyboard_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart' show rootBundle;
 
 abstract class KeyboardService {
   Future<void> currentScreen(String screenId);
