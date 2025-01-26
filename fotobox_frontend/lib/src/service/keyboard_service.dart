7095c38b6dcaf977224b267ddeac40ddf8f39c1d
fseitz <fabi.seitz@gmail.com>
2025-01-26 21:53:31 +0100

    fix loading of config

--- fotobox_frontend/lib/src/service/keyboard_service.dart
+++ fotobox_frontend/lib/src/service/keyboard_service.dart
@@ -10,10 +10,6 @@ abstract class KeyboardService {
 class KeyboardServiceImplementation implements KeyboardService {
   String? _baseUrl;
 
-  KeyboardServiceImplementation() {
-    _init();
-  }
-
   Future<String> _init() async {
     print('KeyboardServiceImplementation: _init');
     var configFile = await rootBundle.loadString('assets/config.json');
@@ -21,24 +17,24 @@ class KeyboardServiceImplementation implements KeyboardService {
     var config = jsonDecode(configFile);
     print('KeyboardServiceImplementation: _init3');
     print('KeyboardServiceImplementation: _init: ${config['keyboardUrl']}');
-    
+
     _baseUrl = config['keyboardUrl'];
     return config['keyboardUrl'];
   }
 
   Future<void> currentScreen(String screenId) async {
     try {
-        var burl;
-        if (_baseUrl == null) {
-            burl = await _init();
-        }
-        print('KeyboardServiceImplementation');
-        var url = Uri.http(burl, '/api/v1/scene/$screenId');
+      if (_baseUrl == null) {
+        await _init();
+      }
+      print('KeyboardServiceImplementation');
+      var url = Uri.http(_baseUrl!, '/api/v1/scene/$screenId');
       print('KeyboardServiceImplementation: currentScreen: $_baseUrl');
       var response = await http.post(url);
 
       if (response.statusCode != 200) {
-        throw Exception('Fehler beim Erstellen der Szene: ${response.statusCode}');
+        throw Exception(
+            'Fehler beim Erstellen der Szene: ${response.statusCode}');
       }
     } catch (e) {
       throw Exception('Fehler beim Erstellen der Szene: $e');
